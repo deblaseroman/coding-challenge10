2d58c405677dcdf893741db6507623e728da9cf1
roman deblase <rdeblase2000@gmail.com>
2024-10-20 23:16:44 -0400

    Create HTML structure for product page

---
+++ main.js
@@ -0,0 +1,8 @@
+const product = {
+    name: "T-Shirt",
+    sizes: {
+        small: {price: 10.00, inStock: true},
+        medium: {price: 12.00, inStock: true},
+        large: {price: 14.00, inStock: false},
+    }
+};
